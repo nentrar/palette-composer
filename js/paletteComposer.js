a22a44de866170012f0e80973880f8605a13d6b1
nentrar <p.semeniuk@gmail.com>
2017-06-23 23:14:50 +0200

    Change functions to each do one action; change functions names

--- js/paletteComposer.js
+++ js/paletteComposer.js
@@ -6,8 +6,16 @@ function getRandomColor() {
     return color;
 }
 
-function changeBackgroundColor() {
+function getRandomBackgroundColor() {
     document.getElementById("background").style.backgroundColor = getRandomColor();
+}
+
+function showColorHex() {
+    document.getElementById("colorNumber").innerHTML = rgbaToHex(); //set random Color as background
+
+    }
+
+function rgbaToHex() {
     var bgColor = document.getElementById("background").style.backgroundColor;
     var tempColor = bgColor.split("(")[1].split(")")[0]; //cut away CSS parts
     tempColor = tempColor.split(",");   //split into separate numbers
@@ -16,6 +24,19 @@ function changeBackgroundColor() {
         return (x.length==1) ? "0"+x : x;  //Add zero if we get only one character
     })
     hexColor = "#"+hexColor.join("");  //glue it back together
-    document.getElementById("colorNumber").innerHTML = hexColor;
+    return hexColor;
+}
 
-    }
+
+
+function getRandomColorUnit() {
+    var letters = '0123456789ABCDEF';
+    var colorUnit = letters[Math.floor(Math.random() * 16)];
+    return colorUnit;
+}
+
+function changeRed() {
+    rgbaToHex();
+
+
+}
